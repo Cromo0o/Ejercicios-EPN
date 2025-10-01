0e9b509000e52fea34e43a9350e96d096f47a0b0
Matthew <matthew.llerena@epn.edu.ec>
2025-10-01 00:45:51 -0500

    Convert Celsius to Fahrenheit using a function

--- cels_to_fahren.c
+++ cels_to_fahren.c
@@ -4,19 +4,18 @@
 
 float conversion(float temp_cels){
 
-    return (1.8 * temp_cels) + 32;
+    return (1.8 * temp_cels) + 32; //Pedimos que se regrese la conversion
 
 }
 
-
 int main(void){
 
-    float temp_cels;
+    float temp_cels; //Definimos esta variable para poder ingresar el dato 
 
     printf("Digita la temperatura actual de Quito en grados celsius: ");
-    scanf("%f", &temp_cels);
+    scanf("%f", &temp_cels); //ingresamos el dato en la variable
 
-    printf("La temperatura de Quito en grados fahrenheit es de: %.2f\n", conversion(temp_cels));
+    printf("La temperatura de Quito en grados fahrenheit es de: %.2f\n", conversion(temp_cels));  //Llamamos a la funcion para que nos de el resultado   
 
     return 0;
 
