8547aad6bd6932e4c2fa5629af60dbea7d3d3881
Matthew <matthew.llerena@epn.edu.ec>
2025-10-01 00:19:48 -0500

    Average of three grades

---
+++ Promedio.c
@@ -0,0 +1,21 @@
+//sacar el promedio de tres notas
+#include <stdio.h>
+
+int main(void){
+
+    float grade1, grade2, grade3, average;
+
+    printf("Digite su primera nota: ");
+    scanf("%f",&grade1);
+
+    printf("Digite su segunda nota: ");
+    scanf("%f",&grade2);
+
+    printf("Digite su tercer nota: ");
+    scanf("%f",&grade3);
+
+    average = (grade1 + grade2 + grade3)/3.0;
+
+    printf("El promedio es %.2f\n",average);
+
+}
