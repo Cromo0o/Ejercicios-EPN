7e3175ee24f16e1c17c7f559f34d244d0bc0982b
Matthew <matthew.llerena@epn.edu.ec>
2025-10-01 00:00:21 -0500

    Crear e imprimir variables

--- Create_and_print.c
+++ Create_and_print.c
@@ -1,1 +1,21 @@
 //Imprimir variables en pantalla
+/*Tipos de variables :    (C es un lenguaje fuertemente tipado)
+
+    int   -> "%d"
+    float -> "%f"
+    double
+    char
+    bool
+    void
+
+*/
+
+#include <stdio.h>
+
+int main(void){
+
+    int var;
+    var = 20;
+    printf("%d",var);
+
+}
--- Hola Mundo.c
+++ Hola Mundo.c
@@ -2,7 +2,7 @@
 
 #include <stdio.h>
 
-int main(){
+int main(void){
 
     printf("Hola mundo, soy Matthew");
     return 0;
