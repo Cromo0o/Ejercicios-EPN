247b403c4045c6a60c20e23417bff0a4d428de4f
Matthew <matthew.llerena@epn.edu.ec>
2025-10-02 18:12:44 -0500

    Knowing whether a number is even or odd

---
+++ Impar_Par_Numero.c
@@ -0,0 +1,22 @@
+//saber si un numero es par o impar
+#include <stdio.h>
+
+int main(void){
+
+    int num;
+
+    printf("Digite un numero para saber si es par o impar: ");
+    scanf("%d",&num);
+
+    if(num % 2 == 0){
+
+        printf("El numero %d es par :)",num);
+
+    }
+    else{
+
+        printf("El numero %d es impar :(",num);
+
+    }
+
+}
