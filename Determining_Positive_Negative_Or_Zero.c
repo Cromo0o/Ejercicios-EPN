d120fd92a1b01f0d52fcda0ed300b9021c104499
Matthew <matthew.llerena@epn.edu.ec>
2025-10-03 21:06:02 -0500

    Determining whether an integer is positive, negative, or zero.

---
+++ Determining_Positive_Negative_Or_Zero.c
@@ -0,0 +1,27 @@
+//Determining whether an integer is positive, negative, or zero. (Spanish)
+
+#include <stdio.h>
+
+int main(void){
+
+    int num; //Definimos una variable como un numero entero, será la que almacene el numero a tratar: num
+
+    printf("Digite el numero a comprobar: ");   //Pedimos el numero al usuario y lo guardamos en la variable anteriormente creada.
+    scanf("%d",&num);
+
+    if(num>0){
+
+        printf("El numero %d es positivo",num);     //Si el numero es mayor que cero, imprimir "El numero es positivo".
+
+    }
+    else if(num<0){
+
+        printf("El numero %d es negativo",num);     //Si el numero es menor que cero, imprimir "El numero es negativo".
+
+    }
+    else{
+
+        printf("El numero es cero");     //Si no se cumple el paso 3 y 4, imprimir "El numero es cero".
+
+    }
+}
