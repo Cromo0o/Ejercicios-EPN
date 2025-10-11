6a1fbd4296381e41ca425dd6e8d25df724c0d789
Matthew <matthew.llerena@epn.edu.ec>
2025-10-11 11:17:55 -0500

    Calculate the hypotenuse of a right triangle

---
+++ HypotenuseTriangle.c
@@ -0,0 +1,23 @@
+//Calculate the hypotenuse of a right triangle given two legs.
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+
+    float legA, legB, hypC;
+
+    printf("* Calculate the hypotenuse of a right triangle *\n");
+
+    printf("Enter the first leg of your triangle in cm: ");
+    scanf("%f",&legA);
+
+    printf("Enter the second leg of your triangle in cm: ");
+    scanf("%f",&legB);
+
+    hypC = sqrt( (legA * legA) + (legB * legB) );
+    
+    printf("The hypotenuse of your triangle is: %.2f cm\n", hypC);
+    
+    return 0;
+
+}
