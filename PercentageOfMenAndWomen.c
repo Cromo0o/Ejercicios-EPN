44c3265cd2625a143acb8ff4462a7fbaa6010055
Matthew <matthew.llerena@epn.edu.ec>
2025-10-10 23:49:52 -0500

    Determine the percentage of men and women in a given group

--- PercentageOfMenAndWomen.c
+++ PercentageOfMenAndWomen.c
@@ -1,23 +1,25 @@
 #include <stdio.h>
-
+//Determine the percentage of men and women in a given group
 int main(void){
 
-    int  hmbrs, mjrs, total;
-    float percentH, percentM;
+    float hmbrs, mjrs, total, percentH, percentM;
 
     printf("* Programa para saber el porcentaje de hombres y mujeres en un grupo *\n");
 
     printf("Digite la cantidad de mujeres: ");
-    scanf("%d",&mjrs);
+    scanf("%f",&mjrs);
 
     printf("Digite la cantidad de hombres: ");
-    scanf("%d",&hmbrs);
+    scanf("%f",&hmbrs);
 
     total = hmbrs + mjrs;
-    percentH = ( hmbrs / total ) * 100;
-    percentM = ( mjrs / total ) * 100;
+    percentH = ( ( hmbrs / total ) * 100 );
+    percentM = ( ( mjrs / total ) * 100 );
 
-    printf("En un grupo de %d personas, hay %.2f%% de hombres");
 
+    printf("En un grupo de %.0f personas, hay %.2f%% de hombres\n", total, percentH);
+    printf("En un grupo de %.0f personas, hay %.2f%% de mujeres\n", total, percentM);
+
+    return 0;
 
 }
