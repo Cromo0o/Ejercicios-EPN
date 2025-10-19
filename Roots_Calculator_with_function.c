094dd6207131bb8a43e713f858faf5461b9a9735
Matthew <matthew.llerena@epn.edu.ec>
2025-10-18 19:19:04 -0500

    Program to calculate the square root and cube root of a number

--- Roots_Calculator_with_function.c
+++ Roots_Calculator_with_function.c
@@ -12,7 +12,7 @@ float C_root(float n){
 int main(void){
 
     float n, C_Root_Negative;     // n = numero   C_Root_Negative = Raiz cubica negativa   
-    int option;                   // option = opcion deseada
+    int option, repeat;                   // option = opcion deseada    repeat = guardar numero que repita proceso o no
 
     printf("* PROGRAMA PARA CALCULAR LA RAIZ CUADRADA Y CUBICA DE UN NUMERO\n");
     printf("1. Raiz Cuadrada :)\n");
@@ -20,52 +20,55 @@ int main(void){
     printf(">>");
     scanf("%d",&option);
    
-    switch (option) {
+    do {
         
-        case 1:
+        switch (option){
+            
+            case 1:
 
-            printf("Digite el numero a calcular: ");
-            scanf("%f",&n);
+                printf("Digite el numero a calcular: ");
+                scanf("%f",&n);
 
-            if ( n < 0 ){      // Using guard clauses
+                if ( n < 0 ){      // Using guard clauses
 
-                printf("La raiz cuadrada de numeros negativos no esta definida en el conjunto de los numeros reales.\n");
+                    printf("* La raiz cuadrada de numeros negativos no esta definida en el conjunto de los numeros reales. *\n");
+                    
+                }
+                else{
+
+                    printf("La raiz cuadrada de %.0f es %.2f\n", n, sqrt(n));
+
+                }
+
+                break;
+
+            case 2:
                 
-            }
-            else{
+                printf("Digite el numero a calcular: ");
+                scanf("%f",&n);
+                
+                if ( n >= 0){
+                
+                    printf("La raiz cubica de %.0f es %.2f.\n", n, C_root(n));
 
-                printf("La raiz cuadrada de %.0f es %.2f\n", n, sqrt(n));
+                }
+                else{
 
-            }
+                    C_Root_Negative = C_root(n) * (-1);
+                    printf("La raiz cubica de %.0f es %.2f.\n", n, C_Root_Negative);
 
-            break;
+                }
 
-        case 2:
-            
-            printf("Digite el numero a calcular: ");
-            scanf("%f",&n);
-            
-            if ( n >= 0){
-            
-                printf("La raiz cubica de %.0f es %.2f.\n", n, C_root(n));
+                break;
 
-            }
-            else{
-
-                C_Root_Negative = C_root(n) * (-1);
-                printf("La raiz cubica de %.0f es %.2f.\n", n, C_Root_Negative);
-
-            }
-
-            break;
-    
-        default:
+        }
         
-            printf("No vale");
+        printf("Desea volver a repetir el programa? NO = Digite 0, SI = Digite cualquier otro numero\n");
+        printf(">>");
+        scanf("%d",&repeat);
 
-            break;
-    }
-    
+    } while (repeat != 0);
+
     return 0;
 
 }
