6b7258644a25b2b5694f30f2ab021cc92afb8fc2
Matthew <matthew.llerena@epn.edu.ec>
2025-10-20 00:16:12 -0500

    Program to Calculate the square root and cube root of a number

--- Roots_Calculator_with_function.c
+++ Roots_Calculator_with_function.c
@@ -5,78 +5,83 @@
 
 float C_root(float n){
 
-    return exp( (1.0/3.0) * log(fabs(n)) );
+    return exp( (1.0/3.0) * log(fabs(n)) );     //Definicion logartimica de la raiz cubica
 
-}
-
-float sacarParteDecimal(float option2, int option){
-    
-    option = trunc(option2);     //Obtemenos solo el valor entero
-
-    return option;
 }
 
 
 int main(void){
 
-    float n, C_Root_Negative, option2, PD_Option2;     // n = numero   C_Root_Negative = Raiz cubica negativa option = opcion deseada  
-    int repeat, option;                   // repeat = guardar numero que repita proceso o no
+    float n, C_Root_Negative, option2;   // | n = numero | |C_Root_Negative = Raiz cubica negativa| 
+    int repeat, option;         // |repeat = guardar numero que repita proceso o no|     |option = numero (entero) de opcion deseada|
 
     do {
         
-        printf("* PROGRAMA PARA CALCULAR LA RAIZ CUADRADA Y CUBICA DE UN NUMERO\n");
+        printf("* PROGRAMA PARA CALCULAR LA RAIZ CUADRADA Y CUBICA DE UN NUMERO *\n");
         printf("1. Raiz Cuadrada :)\n");
         printf("2. Raiz Cubica :D\n");
         printf(">>");
-        scanf("%d",&option2);
+        scanf("%f",&option2);
+
+        option = (int)option2;
+
+
+        if ( option != 1 && option != 2){
+
+            printf("El numero ingresado no es una opcion valida para el menu.\n");
+
+        }        
         
-        switch (option){
+        else{
             
-            case 1:
+            switch (option){
+                
+                case 1:
 
-                printf("Digite el numero a calcular: ");
-                scanf("%f",&n);
+                    printf("Digite el numero a calcular: ");
+                    scanf("%f",&n);
 
-                if ( n < 0 ){      // Using guard clauses
+                    if ( n < 0 ){      // Using guard clauses
 
-                    printf("* La raiz cuadrada de numeros negativos no esta definida en el conjunto de los numeros reales. *\n");
+                        printf("* La raiz cuadrada de numeros negativos no esta definida en el conjunto de los numeros reales. *\n");
+                        
+                    }
+                    else{
+
+                        printf("La raiz cuadrada de %.0f es %.2f\n", n, sqrt(n));
+
+                    }
+
+                    break;
+
+                case 2:
                     
-                }
-                else{
+                    printf("Digite el numero a calcular: ");
+                    scanf("%f",&n);
+                    
+                    if ( n >= 0){
+                    
+                        printf("La raiz cubica de %.0f es %.2f.\n", n, C_root(n));
 
-                    printf("La raiz cuadrada de %.0f es %.2f\n", n, sqrt(n));
+                    }
+                    else{
 
-                }
+                        C_Root_Negative = C_root(n) * (-1);
+                        printf("La raiz cubica de %.0f es %.2f.\n", n, C_Root_Negative);
 
-                break;
+                    }
 
-            case 2:
-                
-                printf("Digite el numero a calcular: ");
-                scanf("%f",&n);
-                
-                if ( n >= 0){
-                
-                    printf("La raiz cubica de %.0f es %.2f.\n", n, C_root(n));
+                    break;
 
-                }
-                else{
-
-                    C_Root_Negative = C_root(n) * (-1);
-                    printf("La raiz cubica de %.0f es %.2f.\n", n, C_Root_Negative);
-
-                }
-
-                break;
-
+            }
+            
+            printf("Desea volver a repetir el programa? NO = Digite 0, SI = Digite cualquier otro numero\n");
+            printf(">>");
+            scanf("%d",&repeat);
         }
-        
-        printf("Desea volver a repetir el programa? NO = Digite 0, SI = Digite cualquier otro numero\n");
-        printf(">>");
-        scanf("%d",&repeat);
 
     } while (repeat != 0);
-
+    
     return 0;
 
 }
