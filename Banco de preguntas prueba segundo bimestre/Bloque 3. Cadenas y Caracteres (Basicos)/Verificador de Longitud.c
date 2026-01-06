0899f16950d2a580d254e582b29031e4abbecc4d
Matthew <matthew.llerena@epn.edu.ec>
2026-01-06 08:32:45 -0500

    Verificar longitud minima de caracteres en string.

---
+++ Banco de preguntas prueba segundo bimestre/Bloque 3. Cadenas y Caracteres (Basicos)/Verificador de Longitud.c
@@ -0,0 +1,44 @@
+/*
+
+Función que reciba una cadena y verifique si tiene al 
+menos 8 caracteres. Retorna 1 (válida) o 0 (inválida). 
+
+*/
+#include <stdio.h>
+#include <string.h>
+
+int checkMinimum(char str[100]){
+
+    int strLen = strlen(str);
+    int lastChar = strLen - 1;
+
+    if (str[lastChar] == '\n'){
+        str[lastChar] = '\0';
+        strLen = strlen(str);
+    }
+
+    if(strLen >= 8){
+        return 1;
+    }
+
+    return 0;
+}
+
+int main(void){
+
+    char str[100];
+
+    printf("Digite una frase cualquiera:\n>>");
+    fgets(str, sizeof(str), stdin);
+
+    printf("Frase ingresada: %s", str);
+
+    if (checkMinimum(str) == 0){
+        printf("La frase ingresada NO cumple con el minimo de 8 caracteres");
+    }
+    else{
+        printf("La frase ingresada cumple con el minimo de 8 caracteres");
+    }
+
+    return 0;
+}
