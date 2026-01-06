381869c2f3cc03e29374428a9c590d4549515497
Matthew <matthew.llerena@epn.edu.ec>
2026-01-06 07:51:38 -0500

    Contador de vocales totales en una cadena de texto

---
+++ Banco de preguntas prueba segundo bimestre/Bloque 3. Cadenas y Caracteres (Basicos)/Contador de Vocales.c
@@ -0,0 +1,43 @@
+/*
+
+Función que reciba una palabra y cuente cuántas vocales 
+tiene en total. 
+
+*/
+#include <stdio.h>
+#include <string.h>
+
+int countVowels(char string[100], int stringLen){
+
+    const char vowels[] = {'a', 'e', 'i', 'o', 'u'};
+
+    int vowelsCounter = 0;
+
+
+    //Elige las vocales, de a la a a la u
+    for (int i = 0; i < stringLen; i++){
+        
+        //La vocal elegida la compara con los caracteres de la cadena
+        for (int j = 0; j < 5; j++){
+            if(string[i] == vowels[j]){
+                vowelsCounter++;
+            }
+        }
+    }
+    return vowelsCounter;
+}
+
+int main(void){
+
+    char string[100];
+
+    printf("Digite una frase para obtener la cantidad de vocales:\n>>");
+    fgets(string, sizeof(string), stdin);
+
+    int stringLen = strlen(string);
+
+
+    printf("La palabra tiene un total de %d vocales.", countVowels(string, stringLen));
+
+    return 0;
+}
